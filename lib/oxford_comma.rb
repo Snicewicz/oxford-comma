bf46f7b99051ad9d187b0502f09114d8232ebdf6
Snicewicz <snicewicz@gmail.com>
2021-12-07 13:06:39 -0500

    commit tree 91ed8796d62be27e1a2a5f770935e92eee3e5dbe

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,3 +1,11 @@
 def oxford_comma(array)
-
+    array.count()
+    if array.count() <= 1
+        array.join
+    elsif array.count == 2 
+        array.join(" and ")
+    else array.count > 2 
+        stored = array.pop
+        array.join(", ") + ", and " + stored
+end
 end
